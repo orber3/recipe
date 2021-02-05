2486f4722a27ced080f0b041e664587b2a269bae
or berman <orber3@gmail.com>
2021-02-05 15:51:00 +0200

    no comments

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import {createRecipe} from '../controllers/recipeController.js'
+import {createRecipe,getRecipes} from '../controllers/recipeController.js'
 
 
 router.route('/postRecipe')
 .post(createRecipe)
+.get(getRecipes)
 
 export default router
